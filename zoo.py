a62a65f96f1e3168e64716adbb82506c05a052dd
Swity Yadav <119117159+shristi08yadav@users.noreply.github.com>
2022-12-06 21:46:02 +0530

    Add files via upload

--- zoo.py
+++ zoo.py
@@ -1,11 +1,3 @@
-"""
-You are required to enter a word that consists of  and  that denote the number of Zs and Os respectively. The input word is considered similar to word zoo if .
-
-Determine if the entered word is similar to word zoo.
-
-For example, words such as zzoooo and zzzoooooo are similar to word zoo but not the words such as zzooo and zzzooooo.
-Print Yes if the input word can be considered as the string zoo otherwise, print No.
-"""
 s = str(input())
 a = 0
 b = 0
